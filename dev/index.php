b486736dce40262e024a24406a8e5ad7199a0ac0
Matt Mitchell <mmitchell413@gmail.com>
2017-06-19 15:00:48 -0500

    set up basic structure of site

---
+++ dev/index.php
@@ -0,0 +1,104 @@
+<html>
+  <head>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/main.css" type="text/css" />
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+    <!-- Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width" />
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <nav class="main">
+          <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>Listen</a></li>
+            <li><a href='#'>Tour</a></li>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Blog</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <h1>Sunshine Serenade</h1>
+        <hr />
+        <h2>Burn</h2>
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+        	 viewBox="0 0 1000 100" style="background-color:#d1d8e9; height:auto; width:100%;">
+        <style type="text/css">
+        	.st0{fill:#26393F; height:100px; width:100%;}
+        </style>
+        <polygon class="st0" points="0,0 500,100 1000,0 "/>
+        </svg>
+      </div>
+    </header>
+    <main>
+      <section>
+        <div class="container">
+          <div class="row">
+            <div class="col col-100">
+              <h3>Welcome</h3>
+            </div>
+            <div class="col col-50">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat, tellus nec tincidunt laoreet, libero quam finibus elit, consectetur malesuada quam sapien nec turpis. Aenean aliquam nisl et tincidunt venenatis. In molestie massa sed massa mollis, iaculis finibus erat pulvinar. Aliquam laoreet gravida neque sed vehicula. Nunc elementum, tellus eu euismod ultricies, orci odio mollis nibh, non posuere nunc nunc a mauris. Nullam at gravida libero. Nam neque lacus, sodales et ultrices quis, malesuada eu enim. Quisque nec urna vitae eros dapibus rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dapibus sapien at orci volutpat maximus. Etiam vel dolor mauris. Etiam lacinia facilisis tortor eget feugiat. Praesent condimentum suscipit tellus sed pretium. Ut ac sodales neque. Morbi sit amet semper nisl.</p>
+
+  <p>Ut vestibulum ac diam ut interdum. Etiam hendrerit metus et eleifend pretium. Nam molestie facilisis neque, quis rhoncus urna vulputate in. Vestibulum quis diam at turpis sodales posuere eget et augue. Integer justo magna, faucibus ut ex eget, aliquet maximus ipsum. Praesent viverra lacus in fermentum porttitor. Curabitur et luctus massa. Morbi pellentesque eros quam, tempus auctor mauris egestas et. Nullam vel tincidunt augue, vulputate condimentum lectus. Morbi aliquam diam leo, et efficitur tortor blandit ut. Vestibulum luctus nibh et elit elementum, eget posuere leo porta.</p>
+
+  <p>Nunc sed tortor commodo, pharetra nisi ac, sollicitudin urna. Donec leo mi, tempus ut neque et, pretium ullamcorper nibh. Duis blandit nunc quis felis malesuada iaculis. Aliquam ornare lorem lacus. Quisque lacinia ac orci vitae vehicula. In hac habitasse platea dictumst. Etiam ligula dui, commodo sed sem eu, feugiat fermentum dui. Proin dui tortor, posuere quis massa feugiat, eleifend volutpat ligula. Donec sollicitudin erat vel velit rutrum volutpat.</p>
+            </div>
+            <div class="col col-50">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat, tellus nec tincidunt laoreet, libero quam finibus elit, consectetur malesuada quam sapien nec turpis. Aenean aliquam nisl et tincidunt venenatis. In molestie massa sed massa mollis, iaculis finibus erat pulvinar. Aliquam laoreet gravida neque sed vehicula. Nunc elementum, tellus eu euismod ultricies, orci odio mollis nibh, non posuere nunc nunc a mauris. Nullam at gravida libero. Nam neque lacus, sodales et ultrices quis, malesuada eu enim. Quisque nec urna vitae eros dapibus rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dapibus sapien at orci volutpat maximus. Etiam vel dolor mauris. Etiam lacinia facilisis tortor eget feugiat. Praesent condimentum suscipit tellus sed pretium. Ut ac sodales neque. Morbi sit amet semper nisl.</p>
+
+  <p>Ut vestibulum ac diam ut interdum. Etiam hendrerit metus et eleifend pretium. Nam molestie facilisis neque, quis rhoncus urna vulputate in. Vestibulum quis diam at turpis sodales posuere eget et augue. Integer justo magna, faucibus ut ex eget, aliquet maximus ipsum. Praesent viverra lacus in fermentum porttitor. Curabitur et luctus massa. Morbi pellentesque eros quam, tempus auctor mauris egestas et. Nullam vel tincidunt augue, vulputate condimentum lectus. Morbi aliquam diam leo, et efficitur tortor blandit ut. Vestibulum luctus nibh et elit elementum, eget posuere leo porta.</p>
+
+  <p>Nunc sed tortor commodo, pharetra nisi ac, sollicitudin urna. Donec leo mi, tempus ut neque et, pretium ullamcorper nibh. Duis blandit nunc quis felis malesuada iaculis. Aliquam ornare lorem lacus. Quisque lacinia ac orci vitae vehicula. In hac habitasse platea dictumst. Etiam ligula dui, commodo sed sem eu, feugiat fermentum dui. Proin dui tortor, posuere quis massa feugiat, eleifend volutpat ligula. Donec sollicitudin erat vel velit rutrum volutpat.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col col-100">
+            <h3>About</h3>
+          </div>
+          <div class="col col-50">
+            <div class="player">
+              <div class="player-art">
+                <p class="player-title">
+                  You Won't Go Far - Matt Mitchell
+                </p>
+                <img src="img/alb-art.jpg" />
+              </div>
+              <div class="player-bar">
+                <div class="player-bar-complete"></div>
+                <div class="player-bar-left"></div>
+              </div>
+              <div class="player-controls">
+                <div class="player-volume">
+                  <div class="player-volume-slider"></div>
+                </div>
+                <div class="player-rw"></div>
+                <div class="player-play-pause"></div>
+                <div class="player-ff"></div>
+              </div>
+            </div>
+          </div>
+          <div class="col col-50">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat, tellus nec tincidunt laoreet, libero quam finibus elit, consectetur malesuada quam sapien nec turpis. Aenean aliquam nisl et tincidunt venenatis. In molestie massa sed massa mollis, iaculis finibus erat pulvinar. Aliquam laoreet gravida neque sed vehicula. Nunc elementum, tellus eu euismod ultricies, orci odio mollis nibh, non posuere nunc nunc a mauris. Nullam at gravida libero. Nam neque lacus, sodales et ultrices quis, malesuada eu enim. Quisque nec urna vitae eros dapibus rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dapibus sapien at orci volutpat maximus. Etiam vel dolor mauris. Etiam lacinia facilisis tortor eget feugiat. Praesent condimentum suscipit tellus sed pretium. Ut ac sodales neque. Morbi sit amet semper nisl.</p>
+
+<p>Ut vestibulum ac diam ut interdum. Etiam hendrerit metus et eleifend pretium. Nam molestie facilisis neque, quis rhoncus urna vulputate in. Vestibulum quis diam at turpis sodales posuere eget et augue. Integer justo magna, faucibus ut ex eget, aliquet maximus ipsum. Praesent viverra lacus in fermentum porttitor. Curabitur et luctus massa. Morbi pellentesque eros quam, tempus auctor mauris egestas et. Nullam vel tincidunt augue, vulputate condimentum lectus. Morbi aliquam diam leo, et efficitur tortor blandit ut. Vestibulum luctus nibh et elit elementum, eget posuere leo porta.</p>
+
+<p>Nunc sed tortor commodo, pharetra nisi ac, sollicitudin urna. Donec leo mi, tempus ut neque et, pretium ullamcorper nibh. Duis blandit nunc quis felis malesuada iaculis. Aliquam ornare lorem lacus. Quisque lacinia ac orci vitae vehicula. In hac habitasse platea dictumst. Etiam ligula dui, commodo sed sem eu, feugiat fermentum dui. Proin dui tortor, posuere quis massa feugiat, eleifend volutpat ligula. Donec sollicitudin erat vel velit rutrum volutpat.</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
